7edcff88c9bcfb6bdcbac6d5ccfe51c073fe79e7
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-10-28 23:44:33 -0500

    Adding kafka consumer in JUnit test

--- library-events-producer/src/test/java/intg/com/kafka2022/controller/LibraryEventsControllerTest.java
+++ library-events-producer/src/test/java/intg/com/kafka2022/controller/LibraryEventsControllerTest.java
@@ -27,9 +27,11 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // to avoid conflicts
-@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
-@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
-        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3) // this will run kafka embedded for test-scope only
+// Override some properties to use the Embedded kafka servers
+@TestPropertySource(properties =
+        {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+         "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 public class LibraryEventsControllerTest {
 
 
@@ -39,18 +41,24 @@ public class LibraryEventsControllerTest {
     @Autowired
     EmbeddedKafkaBroker embeddedKafkaBroker;
 
+    // This consumer will help us to validate if the messages got posted or not.
     private Consumer<Integer, String> consumer;
 
+    // We are creating and initializing the consumer as part of the setUp method.
     @BeforeEach
     void setUp() {
+        // the configs with the hashmap, the group can be anything you want
         Map<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
         configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        // create the consumer
         consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
         embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
     }
 
     @AfterEach
     void tearDown() {
+        // this will help us to shut down the consumer, or you will have some issues in case ypu have multiple tests
         consumer.close();
     }
 
@@ -79,12 +87,18 @@ public class LibraryEventsControllerTest {
         //then
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
 
+        // this will retrieve any records from the embedded kafka topic
         ConsumerRecords<Integer, String> consumerRecords = KafkaTestUtils.getRecords(consumer);
         //Thread.sleep(3000);
         assert consumerRecords.count() == 1;
         consumerRecords.forEach(record-> {
             String expectedRecord = "{\"libraryEventId\":null,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":123,\"bookName\":\"Kafka using Spring Boot\",\"bookAuthor\":\"Richard\"}}";
             String value = record.value();
+
+            // if this is a match, this means whatever you posted was read by the kafka consumer.
+            // So in a nutshell, what I have done here is that I have instantiated a consumer and that consumer
+            // is pulling the record from the library, and as soon as a producer produces a topic,
+            // it retrieves that record for testing/validation purposes.
             assertEquals(expectedRecord, value);
         });
 
