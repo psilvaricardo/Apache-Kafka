ae545cf36bb99ce6060a03c89d6418d010dc9593
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-11-01 19:18:09 -0500

    Adding JUnit Test for OnSuccess method call

--- library-events-producer/src/test/java/unit/com/kafka2022/producer/LibraryEventProducerUnitTest.java
+++ library-events-producer/src/test/java/unit/com/kafka2022/producer/LibraryEventProducerUnitTest.java
@@ -41,7 +41,7 @@ public class LibraryEventProducerUnitTest {
     LibraryEventProducer eventProducer;
 
     @Test
-    void sendLibraryEvent_Approach2_failure() throws JsonProcessingException, ExecutionException, InterruptedException {
+    void sendLibraryEvent_Approach2_onFailure() throws JsonProcessingException, ExecutionException, InterruptedException {
         //given
         Book book = Book.builder()
                 .bookId(123)
@@ -67,7 +67,7 @@ public class LibraryEventProducerUnitTest {
     }
 
     @Test
-    void sendLibraryEvent_Approach2_success() throws JsonProcessingException, ExecutionException, InterruptedException {
+    void sendLibraryEvent_Approach2_onSuccess() throws JsonProcessingException, ExecutionException, InterruptedException {
         //given
         Book book = Book.builder()
                 .bookId(123)
@@ -79,21 +79,28 @@ public class LibraryEventProducerUnitTest {
                 .libraryEventId(null)
                 .book(book)
                 .build();
+
+        // the producer needs this one
         String record = objectMapper.writeValueAsString(libraryEvent);
+        // this is a mock response for the kafkaTemplate.send method call
         SettableListenableFuture future = new SettableListenableFuture();
 
+        // producer record is needed as part of the sendResult param
         ProducerRecord<Integer, String> producerRecord = new ProducerRecord("library-events", libraryEvent.getLibraryEventId(),record );
+        // record metadata is needed as part of the sendResult param
         RecordMetadata recordMetadata = new RecordMetadata(new TopicPartition("library-events", 1),
                 1,1,System.currentTimeMillis(), 1, 2);
+
+        // we are building the result based on the same object type we have on the Event Producer
         SendResult<Integer, String> sendResult = new SendResult<Integer, String>(producerRecord,recordMetadata);
-
+        // setting the sendResult into the 'future'
         future.set(sendResult);
-        when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
-        //when
 
-        ListenableFuture<SendResult<Integer,String>> listenableFuture =  eventProducer.sendLibraryEvent_Approach2(libraryEvent);
+        //when
+        when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
 
         //then
+        ListenableFuture<SendResult<Integer,String>> listenableFuture =  eventProducer.sendLibraryEvent_Approach2(libraryEvent);
         SendResult<Integer,String> sendResult1 = listenableFuture.get();
         assert sendResult1.getRecordMetadata().partition()==1;
 
