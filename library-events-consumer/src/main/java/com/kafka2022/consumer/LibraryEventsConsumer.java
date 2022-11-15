7f5e5b296263574f5b13b18ab3c7943f8edf2e2f
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-11-15 13:50:00 -0600

    Adding new basic KafKa Consumer

---
+++ library-events-consumer/src/main/java/com/kafka2022/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,16 @@
+package com.kafka2022.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventsConsumer {
+
+    @KafkaListener(topics ={"library-events"})
+    public void onMessage(ConsumerRecord<Integer,String> consumerRecord) {
+        log.info("com.kafka2022.consumer.LibraryEventsConsumer.ConsumerRecord : {}", consumerRecord);
+    }
+}
