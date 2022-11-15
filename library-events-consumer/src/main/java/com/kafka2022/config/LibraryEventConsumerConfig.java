2224d09bfa1eae794dcf0ff54329a21d66f19120
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-11-15 12:56:22 -0600

    Refactored Consumer config class file

---
+++ library-events-consumer/src/main/java/com/kafka2022/config/LibraryEventConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.kafka2022.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventConsumerConfig {
+}
--- library-events-consumer/src/main/java/com/kafka2022/config/LibraryEventConsumerConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.kafka2022.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.annotation.EnableKafka
-
-@Configuration
-@EnableKafka
-class LibraryEventConsumerConfig {
-}
